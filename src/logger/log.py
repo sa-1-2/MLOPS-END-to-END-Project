0df54039b16e80b98de46a7e34edc646fa4cb0ef
Sanchit Singla <sanchitsingla1403@gmail.com>
2024-02-10 23:29:49 +0530

    logger added

--- src/logger/log.py
+++ src/logger/log.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-log_path = os.path.join("src\\logger", "logs")
+log_path = os.path.join(os.getcwd(), "logs")
 
 os.makedirs(log_path, exist_ok=True)
 
